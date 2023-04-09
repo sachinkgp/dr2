599afc7e0f9db9198f99dc8ae0d218ec701cd770
sachin <sachinsinghiitkgp@gmail.com>
2023-04-10 02:01:03 +0530

    basic fe

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Signup from './components/signup';
+import Navbar from './components/navbar';
+import SignIn from './components/signin';
+import AllOrders from './components/orders';
+
+// const Routing = ()=>{
+//   return (
+//     <Routes>
+//       <Route path="/signup" element={<Signup />} />
+//       <Route path="/signin" element={<SignIn />} />      
+//     </Routes>
+//   )
+// }
+
+function App() {
+  return (
+  <BrowserRouter>
+  <Navbar />
+    <Routes>
+        <Route path="/allorders" element={<AllOrders />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </BrowserRouter>      
+  );
+}
+
+export default App;
